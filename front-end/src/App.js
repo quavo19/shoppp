f63de73e21492f4ff46d2111ec9db4350aef9a8a
Donald Akite <donaldakite27@gmail.com>
2023-02-15 19:32:03 +0000

    Add cartitem

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { useSelector } from 'react-redux';
+import {BrowserRouter, Routes, Link, Route} from "react-router-dom";
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
 import ProductScreen from './screens/ProductsScreen';
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               amazona
-            </a>
+          </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+          <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
--- front-end/src/screens/CartScreen.js
+++ front-end/src/screens/CartScreen.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { useParams } from "react-router-dom";
+import { addToCart } from '../actions/cartActions';
 
 export default function CartScreen(props) {
     const params = useParams();
@@ -10,6 +12,12 @@ export default function CartScreen(props) {
     const qty = qtyInUrl?Number(qtyInUrl):1
     ? Number(props.location.search.split('=')[1])
     : 1;
+    const dispatch = useDispatch();
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
   return (
     <div>
       <h1>Cart Screen</h1>
