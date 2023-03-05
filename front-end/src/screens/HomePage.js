90b9a9ab570e2c82c1f5e7e88e26934473952255
Donald Akite <donaldakite27@gmail.com>
2023-03-05 13:25:10 +0000

    Add changes

--- front-end/src/screens/HomePage.js
+++ front-end/src/screens/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Welcome from './Welcome'
 import Comtest from './Comtest';
+import Contact from '../Parts/contact/Contact';
 import FeaturedProducts from './FeaturedProducts';
 
 export default function HomePage() {
@@ -9,6 +10,7 @@ export default function HomePage() {
         <Welcome />
         <FeaturedProducts />
         <Comtest />
+        <Contact />
     </>
   )
 }
